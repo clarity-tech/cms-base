8a8a25acc294482a7f19af673e15ce3cdd3857d0
Kashyap Kumar <66243247+kashyap-kumar@users.noreply.github.com>
2024-07-29 19:16:57 +0530

    fix: remove /user route from api route as not needed

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
 use ClarityTech\Cms\Http\Controllers\ContentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::middleware(config('cms.middlewares.api'))
     ->prefix(config('cms.routes.api_prefix'))
