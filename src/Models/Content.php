955e34b1bfa3c29c4b3a7f6cc7ea43ebb8b1f8f5
Kashyap Kumar <kashyap.claritytech@gmail.com>
2024-09-30 20:29:28 +0530

    fix: add taxonomy_id in fillable variable

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -24,6 +24,7 @@ class Content extends Model implements HasMedia
     public string $state;
 
     protected $fillable = [
+        'taxonomy_id',
         'layout',
         'title',
         'slug',
